42c7cfcae22b59ec8d59005554383d447c30ca95
RakelKim <107423827+RakelKim@users.noreply.github.com>
2022-12-17 03:06:05 +0300

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * print_numbers - Print the numbers since 0 up to 9
-  *
-  * Return: The numbers since 0 up to 9
-  */
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
 void print_numbers(void)
 {
-	int x;
+	int ch;
 
-	for (x = 0; x <= 9; x++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		_putchar(x + '0');
+		_putchar(ch);
 	}
-
 	_putchar('\n');
 }
