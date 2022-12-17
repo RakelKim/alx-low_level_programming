9e7f58ddd9f1a53165e06249f56078bcf38dad32
RakelKim <107423827+RakelKim@users.noreply.github.com>
2022-12-17 03:31:35 +0300

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,35 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * main - prints largest prime factor.
+ * Return: Always 0.
  */
-void print_number(int n)
+
+int main(void)
 {
-	unsigned int m, d, count;
+	long int n, fp;
 
-	if (n < 0)
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
 	{
-		_putchar(45);
-		m = n * -1;
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
 	}
-	else
-	{
-		m = n;
-	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	printf("%ld\n", fp);
+	return (0);
 }
