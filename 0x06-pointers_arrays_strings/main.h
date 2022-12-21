e36009c3e349ada98ffb3f06c66ae2761a501f5d
RakelKim <107423827+RakelKim@users.noreply.github.com>
2022-12-21 19:30:16 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,8 +9,3 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-int _putchar(char);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-
-#endi
