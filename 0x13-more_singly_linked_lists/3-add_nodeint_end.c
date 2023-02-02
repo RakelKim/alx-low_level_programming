48d33314c695712bf71695ae1e2e66c3aa723433
RakelKim <107423827+RakelKim@users.noreply.github.com>
2023-02-02 19:58:08 +0300

    Reverse list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,40 +1,27 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - add a new node at the end
- * of a linked list
+ * reverse_listint - reverses a linked list.
  * @head: head of a list.
- * @n: n element.
  *
- * Return: address of the new element. NUll if it failed.
+ * Return: pointer to the first node.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *new;
-	listint_t *temp;
+	listint_t *p;
+	listint_t *n;
 
-	(void)temp;
+	p = NULL;
+	n = NULL;
 
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-	temp = *head;
-	if (*head == NULL)
+	while (*head != NULL)
 	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = new;
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
 	}
 
+	*head = p;
 	return (*head);
 }
